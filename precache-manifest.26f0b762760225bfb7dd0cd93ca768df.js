7aeeee449a251c0155b8076fd88cddccfa0c7cc6
raniaw <raniaw@web.de>
2019-11-06 14:39:59 +0100

    Updates

---
+++ precache-manifest.26f0b762760225bfb7dd0cd93ca768df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a1e58cf930716b2fb3639f42baf9f2",
+    "url": "/react-redux-increment/index.html"
+  },
+  {
+    "revision": "78629f7c6d6b16e2c9d4",
+    "url": "/react-redux-increment/static/css/main.b2bed162.chunk.css"
+  },
+  {
+    "revision": "1f9e51e9ee34b3217cf0",
+    "url": "/react-redux-increment/static/js/2.026e5556.chunk.js"
+  },
+  {
+    "revision": "78629f7c6d6b16e2c9d4",
+    "url": "/react-redux-increment/static/js/main.05f2fab5.chunk.js"
+  },
+  {
+    "revision": "f4bc0196c3a785061938",
+    "url": "/react-redux-increment/static/js/runtime-main.7e45ab89.js"
+  }
+]);
